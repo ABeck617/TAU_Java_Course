ac688947d44418c3c8455d3ee99603a02f96a03a
Anthony Beckford <abeckford532@gmail.com>
2020-06-09 07:37:16 -0400

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -5,6 +5,11 @@ public class Employee extends Person {
     private String employeeId;
     private String title;
 
+    public Employee(){
+        super("angie");
+        System.out.println("In Employee default constructor");
+    }
+
     public String getEmployeeId() {
         return employeeId;
     }
@@ -20,6 +25,4 @@ public class Employee extends Person {
     public void setTitle(String title) {
         this.title = title;
     }
-
-
 }
